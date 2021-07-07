fa80599556b72edad7909827c1bf899da4860681
Seifbarouni <seif.barouni11@gmail.com>
2021-07-07 18:07:30 +0100

    deleted unused imports

--- src/main/java/seif/app/blaviken/Controllers/PostsController.java
+++ src/main/java/seif/app/blaviken/Controllers/PostsController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import seif.app.blaviken.Data.PostsRepository;
 import seif.app.blaviken.Models.Post;
 import seif.app.blaviken.Services.IPostsService;
 
